df057701e2e76c694540c3cc64a92eeedfbecf35
James Campbell <james.p.campbell@gmail.com>
2019-10-08 22:46:22 -0400

    Minor DAG updates for clarity

--- demo/dags/dag_definition_2.py
+++ demo/dags/dag_definition_2.py
@@ -1,7 +1,5 @@
 from datetime import datetime
 
-import logging
-
 import airflow
 from airflow.operators.python_operator import PythonOperator
 from airflow import DAG
@@ -25,13 +23,26 @@ def validate(data_asset_name, expectation_suite_name, batch_kwargs):
     """
     Perform validations of the previously defined data assets according to their respective expectation suites.
     """
-
-    context: DataContext = DataContext('/Users/james/dev/presentations/201910_nyc_airflow_meetup/project_src/great_expectations')
-    context.run_validation_operator(
+    DataContext().run_validation_operator(
         validation_operator_name='action_list_operator',
         assets_to_validate=[(data_asset_name, expectation_suite_name, batch_kwargs)],
         run_identifier=datetime.utcnow().strftime("%Y%m%dT%H%M%S.%fZ")
     )
+
+
+# Validation task
+raw_validation = PythonOperator(
+    task_id='raw_validation',
+    python_callable=validate,
+    op_kwargs={
+        'data_asset_name': 'demo__dir/default/npidata_pfile',
+        'expectation_suite_name': 'warning',
+        'batch_kwargs': BatchKwargs(
+            path='/data/demo/npidata_pfile/npidata_pfile_20050523-20191001_1.csv'
+        )
+    },
+    dag=dag,
+)
 
 
 def demo_transform(infile, outfile):
@@ -48,25 +59,11 @@ def demo_transform(infile, outfile):
     df.to_csv(outfile)
 
 
-# Validation task
-raw_validation = PythonOperator(
-    task_id='raw_validation',
-    python_callable=validate,
-    op_kwargs={
-        'data_asset_name': 'demo__dir/default/npidata_pfile',
-        'expectation_suite_name': 'warning',
-        'batch_kwargs': BatchKwargs(
-            path='/data/demo/npidata_pfile/npidata_pfile_20050523-20190908_0.csv'
-        )
-    },
-    dag=dag,
-)
-
 transform = PythonOperator(
     task_id='transform',
     python_callable=demo_transform,
     op_kwargs={
-        'infile': '/data/demo/npidata_pfile/npidata_pfile_20050523-20190908_0.csv',
+        'infile': '/data/demo/npidata_pfile/npidata_pfile_20050523-20191001_1.csv',
         'outfile': '/data/demo/npidata_pfile/npidata_pfile_20050523-20190908_100.csv'
     },
     dag=dag
